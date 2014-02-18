6acc6688a1a159ad02b80bdfaf61aa4f0f2a3b48
simone77 <simotessa@gmail.com>
2014-02-18 15:51:29 +0100

    new file:   www/index.html 	new file:   www/server.py 	new file:   www/settings.py

---
+++ www/server.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+import json
+import cgitb
+from datetime import datetime
+import calendar
+import MySQLdb
+
+##
+cgitb.enable() 
+db = MySQLdb.connect(host="localhost",  user="root", passwd="brewingbad",db="test")
+serie = []
+cursor = db.cursor()
+cursor.execute("SELECT * FROM Logtemp")
+
+for row in cursor:
+        #date_object = datetime.strptime(row[0], '%Y-%m-%d %H:%M:%S')
+        date_object = row[0]
+        dt = calendar.timegm(date_object.utctimetuple())
+        lista = [dt*1000,row[1]]#moltiplicato per 1000 per avere millisecondi
+        serie.append(lista)
+db.close()
+a = json.dumps(serie)
+print('Content-Type: text/html\n')
+print(a)
+#{name: 'Winter 2009-2010',data: [[Date.UTC(1970,  9,  9), 0   ],[Date.UTC(1970,  9, 14), 0.15],]}]
---
+++ www/settings.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+
+import cgitb
+import cgi
+import MySQLdb
+##
+cgitb.enable()
+print('Content-Type: text/html\n')
+form = cgi.FieldStorage()
+if(form):
+    update_polling = form.getvalue("polling")
+    update_correction = form.getvalue("correction")
+    db2 = MySQLdb.connect(host="localhost",  user="root", passwd="brewingbad",db="test")
+    cursor = db2.cursor()
+    cursor.execute("""UPDATE Settings set polling=%s, correction=%s where ID=0""",(update_polling,update_correction))
+    db2.commit()
+    print("settings updated!")#TODO se update andato a buon fine
+    db2.close()
+
+
+db = MySQLdb.connect(host="localhost",  user="root", passwd="brewingbad",db="test")
+cursor = db.cursor()
+cursor.execute("SELECT * from Settings")
+#print('Content-Type: text/html\n')
+
+row = cursor.fetchall()[0]
+polling = row[1]
+correction = row[2]
+    
+db.close()
+
+###begin html
+#print('Content-Type: text/html\n')
+
+print("""
+<html>
+
+<head><title>Settings</title></head>
+
+<body>
+  <form method="get" action="settings2.py">
+    <p>Intervallo polling: <input type="text" name="polling" value="%s"/> secondi</p>
+    <p>Correzione: <input type="text" name="correction" value="%s"/> gradi</p>
+    <input type="submit" value="Submit"> 
+ </form>
+<a href="index.html">Home</a>
+</body>
+</html>
+""" % (polling, correction))
